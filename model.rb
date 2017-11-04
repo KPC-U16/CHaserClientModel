97c94f29e3e0f5dc753eec4f55b5eacc637b3e6b
Katchan <no1fushi@live.jp>
2017-11-04 16:16:07 +0900

    Methodization

--- model.rb
+++ model.rb
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-require 'CHaserConnect.rb'
+require 'CHaserConnect.rb' #呼び出すおまじない
 
 #　書き換えない
 target = CHaserConnect.new("model") # ()の中好きな名前
 values = Array.new(10)
 random = Random.new # 乱数生成
+
 # ループ前変数
 initPosi = nil # 初期値判定 0=左上 1=左下 2=右下 3=右上
 i = 0 # for文
@@ -14,6 +15,7 @@ wallCountB = 0 # 壁B測定一回目
 itemCountB = 0 # アイテムB測定二回目
 judgA = 0 # 初期移動方向判定一回目
 judgB = 0 # 初期移動方向判定二回目
+
 #ループ内変数
 tar = nil #　Walk,Put,Lookの分岐　0=put,1=walk,2=look
 put = nil # Put 0=Up,1=Left,2=Down,3=Right
@@ -21,160 +23,144 @@ look = nil # Look 0=Up,1=Left,2=Down,3=Right
 go = nil # 移動先 0=Up,1=Left,2=Down,3=Right
 tarn = 4 # ターンカウント(初期移動の分の4)
 
-# 初期位置把握
-values = target.getReady
-values = target.searchUp
-if values[9] == 2
-	values = target.getReady
-	values = target.searchLeft
-    if values[9] == 2
-		intPosi = 0
-	else
-		intPosi = 3
-	end
-else
-	values = target.getReady
-	values = target.searchDown
-	if values[9] == 2
-		values = target.getReady
-		values = target.searchRight
-		if values[9] == 2
-			intPosi = 2
-		else
-			intPosi = 1
-		end
-	end
-end
+#ここからメソッド定義
 
-# マップ左上にいた時の行動
-if intPosi == 0
-	values = target.getReady
-	values = target.lookDown
-	for i in 1..9
-          if values[i] == 2
-          	wallCountA += 1
-          elsif values[i] == 3
-          	itemCountA += 1
-          end
-    end
-
-    values = target.getReady
-	values = target.lookRight
-	for i in 1..9
-          if values[i] == 2
-          	wallCountB += 1
-          elsif values[i] == 3
-          	itemCountB += 1
-          end
-    end
-    wallCountA - item_countA = judgA
-    wallCountB - item_countB = judgB
-    if judgA < judgB
-    	go = 3
-    else
-    	go = 2
-    end
-
-# マップ左下にいた時の行動
-elsif intPosi == 1
-	values = target.getReady
-	values = target.lookUp
-	for i in 1..9
-          if values[i] == 2
-          	wallCountA += 1
-          elsif values[i] == 3
-          	itemCountA += 1
-          end
-    end
-
-    values = target.getReady
-	values = target.lookRight
-	for i in 1..9
-          if values[i] == 2
-          	wallCountB += 1
-          elsif values[i] == 3
-          	itemCountB += 1
-          end
-    end
-    wallCountA - item_countA = judgA
-    wallCountB - item_countB = judgB
-    if judgA < judgB
-    	go = 3
-    else
-    	go = 0
-    end
-
-# マップ右下にいた時の行動
-elsif intPosi == 2
-	values = target.getReady
-	values = target.lookUp
-	for i in 1..9
-          if values[i] == 2
-          	wallCountA += 1
-          elsif values[i] == 3
-          	itemCountA += 1
-          end
-    end
-
-    values = target.getReady
-	values = target.lookLeft
-	for i in 1..9
-          if values[i] == 2
-          	wallCountB += 1
-          elsif values[i] == 3
-          	itemCountB += 1
-          end
-    end
-    wallCountA - item_countA = judgA
-    wallCountB - item_countB = judgB
-    if judgA < judgB
-    	go = 1
-    else
-    	go = 0
-    end
-
-# マップ右上にいた時の行動
-elsif intPosi == 3
-	values = target.getReady
-	values = target.lookDown
-	for i in 1..9
-          if values[i] == 2
-          	wallCountA += 1
-          elsif values[i] == 3
-          	itemCountA += 1
-          end
-    end
-
-    values = target.getReady
-	values = target.lookLeft
-	for i in 1..9
-          if values[i] == 2
-          	wallCountB += 1
-          elsif values[i] == 3
-          	itemCountB += 1
-          end
-    end
-    wallCountA - item_countA = judgA
-    wallCountB - item_countB = judgB
-    if judgA < judgB
-    	go = 1
-    else
-    	go = 2
-    end
-end
-
-
-loop do # ここからループ
-
-#-----ここから-----
+def _initialPositionGrasp(values, target , initPosi) # 初期位置把握
   values = target.getReady
-  if values[0] == 0
-    break
+  values = target.searchUp
+  if values[9] == 2
+	  values = target.getReady
+	  values = target.searchLeft
+    if values[9] == 2
+	 	  initPosi = 0
+	  else
+	 	  initPosi = 3
+    end
+  else
+	  values = target.getReady
+	  values = target.searchDown
+	  if values[9] == 2
+	  	values = target.getReady
+	  	values = target.searchRight
+	  	if values[9] == 2
+	  		initPosi = 2
+	  	else
+	  		initPosi = 1
+	  	end
+	  end
   end
-#-----ここまで書き換えない-----
+end
 
-# アイテムが斜めにあった時
-  if values[1] == 3
+def _initialAction(values, target, initPosi, i,wallCountA, wallCountB, itemCountA ,itemCountB, judgA, judgB, go) # 初期行動
+  if initPosi == 0 # マップ左上にいた時の行動
+	  values = target.getReady
+	  values = target.lookDown
+	  for i in 1..9
+      if values[i] == 2
+      	wallCountA += 1
+      elsif values[i] == 3
+      	itemCountA += 1
+      end
+    end
+    values = target.getReady
+    values = target.lookRight
+	  for i in 1..9
+      if values[i] == 2
+      	wallCountB += 1
+      elsif values[i] == 3
+      	itemCountB += 1
+      end
+    end
+    wallCountA - item_countA = judgA
+    wallCountB - item_countB = judgB
+    if judgA < judgB
+    	go = 3
+    else
+    	go = 2
+    end
+  elsif  initPosi == 1 # マップ左下にいた時の行動
+	  values = target.getReady
+	  values = target.lookUp
+	  for i in 1..9
+      if values[i] == 2
+      	wallCountA += 1
+      elsif values[i] == 3
+      	itemCountA += 1
+      end
+    end
+    values = target.getReady
+	  values = target.lookRight
+	  for i in 1..9
+      if values[i] == 2
+      	wallCountB += 1
+      elsif values[i] == 3
+      	itemCountB += 1
+      end
+    end
+    wallCountA - item_countA = judgA
+    wallCountB - item_countB = judgB
+    if judgA < judgB
+    	go = 3
+    else
+    	go = 0
+    end
+  elsif initPosi == 2 # マップ右下にいた時の行動
+	  values = target.getReady
+	  values = target.lookUp
+	  for i in 1..9
+      if values[i] == 2
+      	wallCountA += 1
+      elsif values[i] == 3
+      	itemCountA += 1
+      end
+    end
+    values = target.getReady
+	  values = target.lookLeft
+	  for i in 1..9
+      if values[i] == 2
+      	wallCountB += 1
+      elsif values[i] == 3
+      	itemCountB += 1
+      end
+    end
+    wallCountA - item_countA = judgA
+    wallCountB - item_countB = judgB
+    if judgA < judgB
+    	go = 1
+    else
+    	go = 0
+    end
+  elsif  initPosi == 3 # マップ右上にいた時の行動
+	  values = target.getReady
+	  values = target.lookDown
+	  for i in 1..9
+      if values[i] == 2
+      	wallCountA += 1
+      elsif values[i] == 3
+      	itemCountA += 1
+      end
+    end
+    values = target.getReady
+	  values = target.lookLeft
+	  for i in 1..9
+      if values[i] == 2
+      	wallCountB += 1
+      elsif values[i] == 3
+      	itemCountB += 1
+      end
+    end
+    wallCountA - item_countA = judgA
+    wallCountB - item_countB = judgB
+    if judgA < judgB
+    	go = 1
+    else
+    	go = 2
+    end
+  end
+end
+
+def _obliqueItemGet(values,  random, go, tar) #斜めのアイテムを取りに行く
+ if values[1] == 3
     rand = random.rand(0..1)
     if rand == 0
       go = 0 # 上に歩く
@@ -191,60 +177,77 @@ loop do # ここからループ
         tar = 1 # walkする
       end
     end
-    elsif values[3] == 3
-      rand = random.rand(0..1)
-      if rand == 0
+  elsif values[3] == 3
+    rand = random.rand(0..1)
+    if rand == 0
+      go = 0 # 上に歩く
+      tar = 1 # walkする
+      if values[2] == 2
+        go = 3 # 右に歩く
+        tar = 1 # walkする
+      end
+    else
+      go = 3 # 右に歩く
+      tar = 1 #walkする
+      if values[6] == 2
         go = 0 # 上に歩く
         tar = 1 # walkする
-        if values[2] == 2
-          go = 3 # 右に歩く
-          tar = 1 # walkする
-        end
-      else
-        go = 3 # 右に歩く
-        tar = 1 #walkする
-        if values[6] == 2
-          go = 0 # 上に歩く
-          tar = 1 # walkする
-        end
-      end
-    elsif values[7] == 3
-      rand = random.rand(0..1)
-      if rand == 0
-        go = 1 # 左に歩く
-        tar = 1 # walkする
-        if values[4] == 2
-          go = 2 # 下に歩く
-          tar = 1 # walkする
-        end
-      else
-        go = 2 # 下に歩く
-        tar = 1 # walkする
-        if values[8] == 2
-          go = 1 # 左に歩く
-          tar = 1 # walkする
-        end
-      end
-    elsif values[9] == 3
-      rand = random.rand(0..1)
-      if rand == 0
-        go = 2 # 下に歩く
-        tar = 1 # walkする
-        if values[8] == 2
-          go = 3 # 右に歩く
-          tar = 1 # walkする
-        end
-      else
-        go = 3 # 右に歩く
-        tar = 1  # walkする
-        if values[6] == 2
-          go = 2 # 下に歩く
-          tar = 1  # walkする
-        end
       end
     end
+    elsif values[7] == 3
+    rand = random.rand(0..1)
+    if rand == 0
+      go = 1 # 左に歩く
+      tar = 1 # walkする
+      if values[4] == 2
+        go = 2 # 下に歩く
+        tar = 1 # walkする
+      end
+    else
+      go = 2 # 下に歩く
+      tar = 1 # walkする
+      if values[8] == 2
+        go = 1 # 左に歩く
+        tar = 1 # walkする
+      end
+    end
+  elsif values[9] == 3
+    rand = random.rand(0..1)
+    if rand == 0
+      go = 2 # 下に歩く
+      tar = 1 # walkする
+      if values[8] == 2
+        go = 3 # 右に歩く
+        tar = 1 # walkする
+      end
+    else
+      go = 3 # 右に歩く
+      tar = 1  # walkする
+      if values[6] == 2
+        go = 2 # 下に歩く
+        tar = 1  # walkする
+      end
+    end
+  end
+end
 
-# 斜めに敵がいたとき
+def _itemGet(values, go, tar) # 隣接するアイテムを取りに行く
+  if values[2] == 3
+    go = 0 # 上に歩く
+    tar = 1 # walkする
+  elsif values[4] == 3
+    go = 1 #　左に歩く
+    tar = 1 # walkする
+  elsif values[6] == 3
+    go = 3 #右に歩く
+    tar = 1 # walkする
+  elsif values[8] == 3
+    go = 2 #下に歩く
+    tar = 1 # walkする
+  end
+end
+
+def _obliqueEnemy(values, random, go, tar, look) #斜めに敵がいた時の行動
   if values[1] == 1
     rand = random.rand(0..1)
     if rand == 0
@@ -346,7 +349,25 @@ loop do # ここからループ
         end
       end
   end
-# 壁除け
+end
+
+def _enemy(values, put, tar) # 隣接した敵に攻撃する
+  if values[2] == 1
+    put = 0 #上にputする
+    tar = 0 # putする
+  elsif values[4] == 1
+    put = 1 #左にputする
+    tar = 0 # putする
+  elsif values[6] == 1
+    put = 2 #下にputする
+    tar = 0 # putする
+  elsif values[8] == 1
+    put = 3 #右にputする
+    tar = 0 # putする
+  end
+end
+
+def _avoidBlock(values, random, go, tar) # 壁にめり込まない
   if go == 0 # 上を向いているとき
     if values[2] == 2
       rand = random.rand(0..1)
@@ -466,38 +487,9 @@ loop do # ここからループ
     end
     tar = 1 # walkする
   end
+end
 
-# アイテムを取りに行く
-  if values[2] == 3
-    go = 0 # 上に歩く
-    tar = 1 # walkする
-  elsif values[4] == 3
-    go = 1 #　左に歩く
-    tar = 1 # walkする
-  elsif values[6] == 3
-    go = 3 #右に歩く
-    tar = 1 # walkする
-  elsif values[8] == 3
-    go = 2 #下に歩く
-    tar = 1 # walkする
-  end
-
-# cher
-  if values[2] == 1
-    put = 0 #上にputする
-    tar = 0 # putする
-  elsif values[4] == 1
-    put = 1 #左にputする
-    tar = 0 # putする
-  elsif values[6] == 1
-    put = 2 #下にputする
-    tar = 0 # putする
-  elsif values[8] == 1
-    put = 3 #右にputする
-    tar = 0 # putする
-  end
-
-# target
+def _act(put, go, look, tar, tarn, values, target) # 行動する
   case tar
     when 0
       case put
@@ -545,12 +537,40 @@ loop do # ここからループ
         tarn += 1
       end
   end
+end
 
-#-----Non-rewritable-----
+# ここから実行するメソッドを書いていく
+
+_initialPositionGrasp(values, target , initPosi) # 初期位置把握
+
+_initialAction(values, target, initPosi, i, wallCountA, wallCountB, itemCountA ,itemCountB, judgA, judgB, go) # 初期行動
+
+loop do # ここからループ
+
+#---------ここから---------
+  values = target.getReady
+  if values[0] == 0
+    break
+  end
+#-----ここまで書き換えない-----
+
+_obliqueItemGet(values, random, go, tar) #斜めのアイテムを取りに行く
+
+_obliqueEnemy(values, random, go, tar, look) #斜めに敵がいたときの行動
+
+_avoidBlock(values, random, go, tar) # 壁にめり込まない
+
+_itemGet(values, go, tar) # 隣接するアイテムを取りに行く
+
+_enemy(values, put, tar) # 隣接する敵に攻撃を仕掛ける
+
+_act(put, go, look, tar, tarn, values, target) #行動する
+
+#---------ここから---------
   if values[0] == 0
     break
   end
 
-end #loop end
+end # ループここまで
 target.close
-#-----Non-rewritable-end-----
+#-----ここまで書き換えない-----
